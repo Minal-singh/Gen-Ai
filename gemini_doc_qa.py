25a22d316c3b9e15443585febe0e12e0fa94f5e9
Minal-singh <rishabhthkur15102@gmail.com>
2025-02-13 23:54:58 +0530

    NEW: Added doc QA chatbot

---
+++ gemini_doc_qa.py
@@ -0,0 +1,69 @@
+import os
+
+from dotenv import load_dotenv
+import streamlit as st
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+
+load_dotenv()
+
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGSMITH_TRACING"] = os.getenv("LANGSMITH_TRACING")
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash", temperature=0, max_tokens=1000, timeout=2, max_retries=2
+)
+
+st.title("LangChain Gemini 2.0 Document QA")
+
+prompt = ChatPromptTemplate.from_template(
+    """
+Answer the following question based on the context provided.:
+<context>
+{context}
+<context>
+Question: {input}
+"""
+)
+
+
+def vector_embedding():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings = GoogleGenerativeAIEmbeddings(
+            model="models/text-embedding-004",
+        )
+        st.session_state.loader = PyPDFDirectoryLoader("pdf/alexa.pdf")
+        st.session_state.docs = st.session_state.loader.load()
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=100
+        )
+        st.session_state.final_documents = (
+            st.session_state.text_splitter.split_documents(st.session_state.docs)
+        )
+        st.session_state.vectors = FAISS.from_documents(
+            st.session_state.final_documents, st.session_state.embeddings
+        )
+
+
+prompt1 = st.text_input("Enter your question here:")
+
+if st.button("Create Vector Store"):
+    vector_embedding()
+    st.write("Vector Store Created")
+
+if prompt1:
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = st.session_state.vectors.as_retriever()
+    retrieval_chain = create_retrieval_chain(document_chain, retriever)
+
+    response = retrieval_chain.invoke({"input": prompt1})
+    st.write(response["answer"])
+
+    st.write(response["context"])
